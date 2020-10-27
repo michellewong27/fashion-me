06050337eb318ec29988febd07b39b00842b3008
Michelle Wong <michellekwong2@gmail.com>
2020-10-27 13:09:26 -0400

    Updated img db.json

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
+const content = document.getElementById("content");
+
+function getImgs(){
+  fetch(`http://localhost:3000/imgs`)
+  .then(function (body){
+    return body.json();
+  })
+  .then(function (img) {
+    console.log(img)
+    // img.forEach()
+  })
+}
+
+function createHomeImg(){
+  const imgDiv = document.createElement("div");
+  imgDiv.className = "img-div";
+  content.appendChild(imgDiv);
+
+  const img = document.createElement("img")
+
+}
