8f8e1df2227bdea2b0bff38cc131b88f538e48e8
Michelle Wong <michellekwong2@gmail.com>
2020-10-27 14:11:40 -0400

    Updated imgs & headlines in db.json

--- index.js
+++ index.js
@@ -16,22 +16,25 @@ function getImgs(){
     return body.json();
   })
   .then(function (img) {
-    img.forEach(createHomeImg);
+    // img.forEach(createHomeImg);
+    img.forEach(renderImg);
     img.forEach(createRadioBtn);
   })
 }
 getImgs();
 
-function createHomeImg(img){
-  const imgDiv = document.createElement("div");
-  imgDiv.className = "img-div";
-  content.appendChild(imgDiv);
-  const imgElement = document.createElement("img")
-  if(img.headline){
-    imgElement.src = `${img.src}`
-  }
-  imgDiv.appendChild(imgElement);
-}
+// function createHomeImg(img){
+//   const imgDiv = document.createElement("div");
+//   imgDiv.className = "img-div";
+//   content.appendChild(imgDiv);
+//   const imgElement = document.createElement("img")
+//   if(img.headline){
+//     imgElement.src = `${img.src}`
+//   }
+//   imgDiv.appendChild(imgElement);
+// }
+
+
 
 //creates the slider divs then calls on creating radio btn
 // function renderImgSlider(){
@@ -41,15 +44,26 @@ function createHomeImg(img){
   const slidesDiv = document.createElement("div");
   slidesDiv.className = "slides";
   sliderDiv.appendChild(slidesDiv);
+  const radioInputsDiv = document.createElement("div");
+  radioInputsDiv.className = "radio-inputs";
 // }
 // renderImgSlider()
 
 function createRadioBtn(img){
-  
   const radioBtn = document.createElement("input");
   radioBtn.type = "radio";
   radioBtn.name = "radio-btn";
   radioBtn.id = `radio${img.id}`;
   slidesDiv.appendChild(radioBtn);
-  console.log(radioBtn);
 }
+
+//has to go after radio btns
+//creating divs & img tags for each img
+function renderImg(img){
+  const slideDiv = document.createElement("div");
+  slideDiv.className = `slide${img.id}`;
+  slidesDiv.appendChild(slideDiv);
+  const imgTag = document.createElement("img");
+  imgTag.src = `${img.src}`;
+  slideDiv.appendChild(imgTag);
+};
